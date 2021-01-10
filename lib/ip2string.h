1b011347862674fa1e3e0641957a6b572954bc88
Vladimir Lazarev <soloc@mail.ru>
2021-01-11 00:25:59 +0300

    Тестирование ip2string.

--- lib/ip2string.h
+++ lib/ip2string.h
@@ -29,112 +29,118 @@
 #include <string>
 
 namespace z80 {
-    /**
-     * @brief Преобразование представления IP-адреса в виде целого числа - в строку.
-     * @tparam T Целочисленный тип.
-     * @param ip Значение типа T, которое следует преобразовать в строку.
-     * @return Для целочисленного T функция возвращает объект типа std::string.
-     * Для T других типов данный вариант функции не скомпилируется.
-     */
-    template<typename T>
-    std::enable_if_t<std::is_integral_v<T>, std::string>
-    ip2string(T ip) {
-        constexpr auto bytes = sizeof(T);
-        std::string rtn;
-        auto ip_ = ip;
-        for (std::size_t byte = 0; byte < bytes; ++byte, ip_ >>= 8) {
-            auto byteValue = ip_ & 0xFF;
-            rtn =  ((bytes - byte) > 1 ? "." : "") + std::to_string(byteValue) + rtn;
-        }
-        return rtn;
+  /**
+   * @brief Преобразование представления IP-адреса в виде целого числа - в строку.
+   * @tparam T Целочисленный тип.
+   * @param ip Значение типа T, которое следует преобразовать в строку.
+   * @return Для целочисленного T функция возвращает объект типа std::string.
+   * Для T других типов данный вариант функции не скомпилируется.
+   */
+  template<typename T>
+  std::enable_if_t<std::is_integral_v<T>, std::string>
+  ip2string(T ip) {
+    constexpr auto bytes = sizeof(T);
+    std::string rtn;
+    auto ip_ = ip;
+    for (std::size_t byte = 0; byte < bytes; ++byte, ip_ >>= 8) {
+      auto byteValue = ip_ & 0xFF;
+      rtn = ((bytes - byte) > 1 ? "." : "") + std::to_string(byteValue) + rtn;
     }
+    return rtn;
+  }
 
-    /**
-     * @brief Вариант "преобразования" IP-адреса в строку из... строки.
-     * На самом деле никакого преобразованяи не производится. Функция возвращает строку, переданную параметром.
-     * @tparam T std::string
-     * @param ip Константная ссылка на строку. Функция не анализирует содержимое переданной строки и возвращает её как есть.
-     * @return Для T = std::string функция возвращает объект типа std::string.
-     * Для T других типов данный вариант функции не скомпилируется.
-     */
-    template<typename T>
-    std::enable_if_t<std::is_same_v<T, std::string>, std::string>
-    ip2string(const T& ip) {
-        return ip;
-    }
+  /**
+   * @brief Вариант "преобразования" IP-адреса в строку из... строки.
+   * На самом деле никакого преобразованяи не производится. Функция возвращает строку, переданную параметром.
+   * @tparam T std::string
+   * @param ip Константная ссылка на строку. Функция не анализирует содержимое переданной строки и возвращает её как есть.
+   * @return Для T = std::string функция возвращает объект типа std::string.
+   * Для T других типов данный вариант функции не скомпилируется.
+   */
+  template<typename T>
+  std::enable_if_t<std::is_same_v<T, std::string>, std::string>
+  ip2string(const T &ip) {
+    return ip;
+  }
 
-    /**
-     * @brief Метафункция проверяет наличие в типе T подтипа - константный итератор.
-     * @tparam T Тип, который следует проверить.
-     * @return has_const_iterator::value = true, если тип T содержит требуемый подтип. false - в противном случае.
-     */
-    template<typename T>
-    struct has_const_iterator {
-        using yes = char[1];
-        using no  = char[2];
-        template<typename C>
-        static yes&
-        test (typename C::const_iterator*);
+  /**
+   * @brief Метафункция проверяет наличие в типе T подтипа - константный итератор.
+   * @tparam T Тип, который следует проверить.
+   * @return has_const_iterator::value = true, если тип T содержит требуемый подтип. false - в противном случае.
+   */
+  template<typename T>
+  struct has_const_iterator {
+    using yes = char[1];
+    using no = char[2];
 
-        template<typename C>
-        static no&
-        test (...);
+    template<typename C>
+    static yes &
+    test(typename C::const_iterator *);
 
-        static const bool value = sizeof(test<T>(nullptr)) == sizeof (yes);
-    };
+    template<typename C>
+    static no &
+    test(...);
 
-    /**
-     * @brief Метафункция проверяет отсутствие в типе T функции c_str.
-     * @tparam T Тип, который следует проверить.
-     * @return has_no_c_str = true, если тип Т НЕ содержит функцию c_str. false - в противном случае.
-     */
-    template<typename T>
-    struct has_no_c_str {
-        using yes = char[1];
-        using no  = char[2];
+    static const bool value = sizeof(test<T>(nullptr)) == sizeof(yes);
+  };
 
-        template<typename U>
-        static no&
-        test (decltype(&U::c_str));
+  /**
+   * @brief Метафункция проверяет отсутствие в типе T функции c_str.
+   * @tparam T Тип, который следует проверить.
+   * @return has_no_c_str = true, если тип Т НЕ содержит функцию c_str. false - в противном случае.
+   */
+  template<typename T>
+  struct has_no_c_str {
+    using yes = char[1];
+    using no = char[2];
+
+    template<typename U>
+    static no &
+    test(decltype(&U::c_str));
 
 
-        template<typename U>
-        static yes&
-        test (...);
+    template<typename U>
+    static yes &
+    test(...);
 
-        static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes);
-    };
+    static constexpr bool value = sizeof(test<T>(0)) == sizeof(yes);
+  };
 
-    /**
-     * @brief Преобразование представления IP-адреса из содержимового контейнера - в строку.
-     * @tparam T Тип - контейнер.
-     * @param ip константная ссылка на объект типа T.
-     * @return Для T типа контейнер - возвращает строку, составленную из содержимого контейнера, разделённого точками.
-     * Для типа T не предоставляющего необходимый интерфейс контейнера - данный вариант функции не скомпилируется.
-     * @details Для работы функции требуется наличие константного итератора - это проверяется метафункцией
-     * z80::has_const_iterator<T>.\n
-     * Однако под данный критерий попадает и std::string, который имеет интерфейс практически идентичный контейнерам, но
-     * std::string должен обрабатываться другим вариантом этой функции.\n
-     * Для исключения применения данного варианта функции, std::string отбрасывается метафункцией z80::has_no_c_str<T>,
-     * которая не должна обнаружить наличие метода c_str у типа T: в std::string этот метод есть, у контейнеров - нет.\n
-     * Результаты работы этих двух метафункций: z80::has_const_iterator<T> и z80::has_no_c_str<T> - собираются по "И"
-     * метафункцией std::conjunction.
-     */
-    template<typename T>
-    std::enable_if_t
+  /**
+   * @brief Преобразование представления IP-адреса из содержимового контейнера - в строку.
+   * @tparam T Тип - контейнер.
+   * @param ip константная ссылка на объект типа T.
+   * @return Для T типа контейнер - возвращает строку, составленную из содержимого контейнера, разделённого точками.
+   * Для типа T не предоставляющего необходимый интерфейс контейнера - данный вариант функции не скомпилируется.
+   * @details Для работы функции требуется наличие константного итератора - это проверяется метафункцией
+   * z80::has_const_iterator<T>.\n
+   * Однако под данный критерий попадает и std::string, который имеет интерфейс практически идентичный контейнерам, но
+   * std::string должен обрабатываться другим вариантом этой функции.\n
+   * Для исключения применения данного варианта функции, std::string отбрасывается метафункцией z80::has_no_c_str<T>,
+   * которая не должна обнаружить наличие метода c_str у типа T: в std::string этот метод есть, у контейнеров - нет.\n
+   * Результаты работы этих двух метафункций: z80::has_const_iterator<T> и z80::has_no_c_str<T> - собираются по "И"
+   * метафункцией std::conjunction.
+   */
+  template<typename T>
+  std::enable_if_t
+  <
+    std::conjunction_v
     <
-        std::conjunction_v
-        <
-            z80::has_const_iterator<T>,
-            z80::has_no_c_str<T>
-        >,
-        std::string
-    >
-    ip2string(const T& ip) {
-        std::string rtn;
-        for(const auto& byte: ip)
-            rtn += std::to_string(byte) + ".";
-        rtn.resize(rtn.size() - 1);
-        return rtn;
-    }
+      z80::has_const_iterator<T>,
+      z80::has_no_c_str<T>
+    >,
+    std::string
+  >
+  ip2string(const T &ip) {
+
+    std::string rtn;
+
+    for (const auto &byte: ip)
+      rtn += std::to_string(byte) + ".";
+
+    if (rtn.size() > 0)
+      rtn.resize(rtn.size() - 1);
+
+    return rtn;
+  }
 }
--- test/test_ip2string.cpp
+++ test/test_ip2string.cpp
@@ -4,16 +4,98 @@
 
 #include <boost/test/unit_test.hpp>
 
-#include <map>
 #include <list>
 #include <vector>
 
 #include "../lib/ip2string.h"
 
+using z80::ip2string;
+using std::string;
+using std::vector;
+using std::list;
+
+/**
+ * @brief Пребразовать строку в "IP-адрес" повторами через точку.
+ * @param str Строка, из которой будет состоять IP-адрес.
+ * @param num Количество повторов
+ * @return Строка вида str.str.str... в которой значение параметра str будет повторено num раз через точку.
+ */
+auto str2ip(const string &str, size_t num) {
+  string rtn;
+  for (decltype(num) i = 0; i < num; ++i) rtn += str + ".";
+  rtn.resize(rtn.size() - 1);
+  return rtn;
+}
+
 BOOST_AUTO_TEST_SUITE(_)
 
-//  BOOST_AUTO_TEST_CASE(Char) {
-//    BOOST_REQUIRE_NO_THROW((z80::ip2string<long>(static_cast<char>(-1))));
-//  }
+  BOOST_AUTO_TEST_CASE(Integral) {
+    string res, expect;
+
+    expect = str2ip("255", sizeof(char));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<char>(-1)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("0", sizeof(char));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<char>(0)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("255", sizeof(int));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<int>(-1)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("0", sizeof(int));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<int>(0)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("255", sizeof(long));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<long>(-1)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("0", sizeof(long));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<long>(0)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("255", sizeof(long long));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<long long>(-1)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+
+    expect = str2ip("0", sizeof(long long));
+    BOOST_REQUIRE_NO_THROW(res = ip2string(static_cast<long long>(0)));
+    BOOST_REQUIRE_EQUAL(res, expect);
+  }
+
+  BOOST_AUTO_TEST_CASE(String) {
+    string res;
+    BOOST_REQUIRE_NO_THROW(res = ip2string(string("")));
+    BOOST_REQUIRE_EQUAL(res, "");
+
+    BOOST_REQUIRE_NO_THROW(res = ip2string(string("1.2.3.4")));
+    BOOST_REQUIRE_EQUAL(res, "1.2.3.4");
+  }
+
+  BOOST_AUTO_TEST_CASE(Vector) {
+    string res;
+    BOOST_REQUIRE_NO_THROW(res = ip2string(vector<char>()));
+    BOOST_REQUIRE_EQUAL(res, "");
+
+    BOOST_REQUIRE_NO_THROW(res = ip2string(vector {1, 2, 3, 4}));
+    BOOST_REQUIRE_EQUAL(res, "1.2.3.4");
+
+    BOOST_REQUIRE_NO_THROW(res = ip2string(vector {-1, -2, -3, -4}));
+    BOOST_REQUIRE_EQUAL(res, "-1.-2.-3.-4");
+  }
+
+  BOOST_AUTO_TEST_CASE(List) {
+    string res;
+    BOOST_REQUIRE_NO_THROW(res = ip2string(list<char>()));
+    BOOST_REQUIRE_EQUAL(res, "");
+
+    BOOST_REQUIRE_NO_THROW(res = ip2string(list {1, 2, 3, 4}));
+    BOOST_REQUIRE_EQUAL(res, "1.2.3.4");
+
+    BOOST_REQUIRE_NO_THROW(res = ip2string(list {-1, -2, -3, -4}));
+    BOOST_REQUIRE_EQUAL(res, "-1.-2.-3.-4");
+  }
 
 BOOST_AUTO_TEST_SUITE_END()
